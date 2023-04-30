28497b42163249f11769710bb069b5eb9b7742fb
Polonez1 <darjus.vasiukevic@gmail.com>
2023-05-01 01:45:29 +0300

    renamed taxis_analysis

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-import os
+
+# import os
 
 import sys
-sys.path.append('.\\scripts\\')
-import taxis_analysis_function as MyFn
+
+sys.path.append(".\\scripts\\")
+import taxis_analysis as MyFn
 import seaborn as sns
 
 sns.set(style="ticks", color_codes=True)
 plt.ion()
 
 df = MyFn.load_dataset()
-df = MyFn.filtered_by_date(df, date_range=('2019-02-28','2019-03-31'))
-general_dataframe = (df.pipe(MyFn.add_week_day)
-.pipe(MyFn.get_time_groups)
-.pipe(MyFn.group_by_time_weekdays)
-.pipe(MyFn.calculate_passengers_fare_index)
+df = MyFn.filtered_by_date(df, date_range=("2019-02-28", "2019-03-31"))
+general_dataframe = (
+    df.pipe(MyFn.add_week_day)
+    .pipe(MyFn.get_time_groups)
+    .pipe(MyFn.group_by_time_weekdays)
+    .pipe(MyFn.calculate_passengers_fare_index)
 )
 
-MyFn.show_heatmap(general_dataframe, by='passengers')
+MyFn.show_heatmap(general_dataframe, by="passengers")
 
 plt.show(block=True)
