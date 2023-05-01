55e3da09633b427374af5fa0cee0c47344c3754d
Polonez1 <darjus.vasiukevic@gmail.com>
2023-05-01 21:10:23 +0300

    Updated load_and_save func

--- scripts/load_and_save_data.py
+++ scripts/load_and_save_data.py
@@ -3,19 +3,25 @@ import pandas as pd
 import os
 
 
-def load_dataset() -> pd.DataFrame:
-    """loading test dataset
-
-    Returns:
-        _type_: pd.Dataframe
-    """
-    return sns.load_dataset("taxis")
-
-
-def collect_data():
-    df_list = []
+def get_data_files_list() -> list:
     path = "..\\data\\yellow_trip_data_2022\\"
     files_list = os.listdir(path)
-    df_list = [pd.read_parquet(f"{path}{i}", engine="pyarrow") for i in files_list]
-    df = pd.concat(df_list)
+
+    return files_list
+
+
+def load_data_frame(month: str, year: str = "2022") -> pd.DataFrame:
+    """load data frame by year and month
+
+    Args:
+        year (str): format: yyyy. Defaut: 2022
+        month (str): format: mm
+
+    Returns:
+        _type_: pd.DataFrame
+    """
+    file_name = f"yellow_tripdata_{year}-{month}.parquet"
+    path = "..\\data\\yellow_trip_data_2022\\"
+    df = pd.read_parquet(f"{path}{file_name}", engine="pyarrow")
+
     return df
