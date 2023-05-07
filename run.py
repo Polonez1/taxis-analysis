80fccd1e10f004643c083c5452bf611bb7f6b91f
Polonez1 <darjus.vasiukevic@gmail.com>
2023-05-07 20:16:36 +0300

    updated reirements and add logging

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import argparse
 import main
+import logging
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="get data visualisation")
@@ -19,6 +20,7 @@ if __name__ == "__main__":
             "Filtered by date range (if pass, press Enter), Date range format: (yyyy-mm-dd, yyyy-mm-dd)"
         )
         date_range = input()
+        logging.info("Data load...please wait")
         main.show_heatmap(month=month, by=by, date_range=())
     elif args.run_table:
         print("input month(Format: mm)")
@@ -27,6 +29,7 @@ if __name__ == "__main__":
             "Filtered by date range (if pass, press Enter), Date range format: (yyyy-mm-dd, yyyy-mm-dd)"
         )
         date_range = input()
+        logging.info("Data load...please wait")
         main.show_profit_table(month=month, date_range=())
     else:
         print("Invalid function name")
