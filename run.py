6fd7d8b811bb938f67c9f7c89213fd03ef51d317
Polonez1 <darjus.vasiukevic@gmail.com>
2023-05-07 17:59:29 +0300

    updatet main and run, deleted profit_main.py

--- run.py
+++ run.py
@@ -1,14 +1,32 @@
 import argparse
-
 import main
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="get data visualisation")
-    parser.add_argument("--run_visualisation", type=str, help="main")
+    parser.add_argument(
+        "--run_visualisation", action="store_true", help="run data visualisation"
+    )
+    parser.add_argument("--run_table", action="store_true", help="run profit table")
 
     args = parser.parse_args()
 
-    if args.function_name == "--run_visualisation":
-        main.main()
+    if args.run_visualisation:
+        print("input month(Format: mm)")
+        month = input()
+        print("Show by: tip, passenger, fare, profit_by_passenger ")
+        by = input()
+        print(
+            "Filtered by date range (if pass, press Enter), Date range format: (yyyy-mm-dd, yyyy-mm-dd)"
+        )
+        date_range = input()
+        main.show_heatmap(month=month, by=by, date_range=())
+    elif args.run_table:
+        print("input month(Format: mm)")
+        month = input()
+        print(
+            "Filtered by date range (if pass, press Enter), Date range format: (yyyy-mm-dd, yyyy-mm-dd)"
+        )
+        date_range = input()
+        main.show_profit_table(month=month, date_range=())
     else:
         print("Invalid function name")
